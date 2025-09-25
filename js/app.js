0dec72ebc9af547417f078c2858c55a9c508c22b
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-25 15:06:25 -0300

    Obtendo quantidade comprada

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 function comprar(){
     //Obtendo o tipo do ingresso selecionado
     let tipoIngresso = document.getElementById("tipo-ingresso").value;
+    //Obtendo a quantidade a ser comprada
+    let qtdComprada = parseInt(document.getElementById("qtd").value);
+    console.log(qtdComprada);
 }

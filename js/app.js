49122de5fbbf75408a83076a1c84208c6f6527b9
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-25 15:31:57 -0300

    Adicionando a lógica para compra do ingresso

--- js/app.js
+++ js/app.js
@@ -14,6 +14,28 @@ function comprar(){
     let superiorDisponivel = pegaQuatidade("qtd-superior");
     let inferiorDisponivel = pegaQuatidade("qtd-inferior");
 
+    //Definindo o tipo do ingresso e fazendo o calculo
+    if(tipoIngresso == "pista"){
+        if(qtdComprada > pistaDisponivel){
+            alert("Quantidade indisponível!");
+        }else {
+            compraIngresso(pistaDisponivel, qtdComprada, "qtd-pista");
+        }
+    }else if(tipoIngresso == "superior"){
+        if(qtdComprada > superiorDisponivel){
+            alert("Quantidade indisponível!");
+        }else {
+            compraIngresso(superiorDisponivel, qtdComprada, "qtd-superior");
+        }
+    }else {
+        if(qtdComprada > inferiorDisponivel){
+            alert("Quantidade indisponível!");
+        }else {
+            compraIngresso(inferiorDisponivel, qtdComprada, "qtd-inferior");
+        }
+    }
+
+    document.getElementById("qtd").value = "";
 }
 
 //função para comprar ingresso
